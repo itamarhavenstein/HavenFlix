e9b42ad295a1cbc223726f0d56119378f1474df0
Itamar <itamarhavenstein@hotmail.com>
2020-07-28 22:41:43 -0300

    melhorado carrousel da home, pagina de video novo, pagina de cadastro

--- havenflix/src/components/Menu/index.js
+++ havenflix/src/components/Menu/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Logo from '../../assets/Logo.png';
 import ButtonLink from '../ButtonLink';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
 import './Menu.css';
 
 function Menu() {
     return(
         <nav className="Menu">
-        <a href="/">
+        <Link to="/">
          <img className="Logo"  src={Logo} alr="HavenFlix logo"/>
-        </a>
+        </Link>
 
-        <Button as="a" className="ButtonLink" href="/">
+        <Button as={Link} className="ButtonLink" to="/cadastro/video">
             Novo vídeo
         </Button>
         </nav>
---
+++ havenflix/src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+import {Link} from 'react-router-dom';
+
+function CadastroCategoria(){
+    return (
+        <PageDefault>
+            <h1>Página de Cadastro Categoria</h1>
+             <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+        </PageDefault>
+    )
+}
+
+export default CadastroCategoria;
