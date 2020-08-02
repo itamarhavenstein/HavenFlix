b6d53ba27e031e9d8a2279df69bc017faf1c9055
Itamar <itamarhavenstein@hotmail.com>
2020-08-02 16:40:30 -0300

    melhorando o layout do formulário de cadastro  de categoria

--- havenflix/src/pages/cadastro/Categoria/index.js
+++ havenflix/src/pages/cadastro/Categoria/index.js
@@ -1,41 +1,55 @@
-import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-  const [categorias, setCategorias] = useState([]);
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: '#000',
-  }
-
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
   function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor,
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
-    const { getAttribute, value } = infosDoEvento.target;
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value)
+      infosDoEvento.target.value,
+    );
   }
+
+  useEffect(() => {
+    console.log('alo alo brasil');
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP).then(async (respostaDoServidor) => {
+      const resposta = await respostaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    });
+  }, []);
 
   return (
     <PageDefault>
-      <h1>Página de Cadastro Categoria: {values.nome}</h1>
+      <h1>
+        Página de Cadastro Categoria:
+        {values.nome}
+      </h1>
       <form onSubmit={function handleSubmit(infosDoEvento) {
         infosDoEvento.preventDefault();
         setCategorias([...categorias, values]);
         setValues(valoresIniciais);
-      }}>
-
+      }}
+      >
         <FormField
           label="Nome da categoria:"
           type="text"
@@ -45,39 +59,42 @@ function CadastroCategoria() {
         />
         <FormField
           label="Descrição:"
-          type="text"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
         />
-        <FormField 
+        <FormField
           label="Cor:"
           type="color"
           name="cor"
           value={values.cor}
           onChange={handleChange}
         />
-
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={categoria + indice}>
-              {categoria.nome}
-            </li>
-          )
-        })}
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
       </ul>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
--- havenflix/src/pages/cadastro/Video/index.js
+++ havenflix/src/pages/cadastro/Video/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import {Link} from 'react-router-dom';
 
-function CadastroVideo(){
-    return (
-        <PageDefault>
-            <h1>Página de Cadastro de Video</h1>
-            <Link to='/cadastro/categoria'>
-                Cadastro Categoria
-            </Link>
-        </PageDefault>
-    )
+function CadastroVideo() {
+  return (
+    <PageDefault>
+      <h1>Página de Cadastro de Video</h1>
+      <Link to="/cadastro/categoria">
+        Cadastro Categoria
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
